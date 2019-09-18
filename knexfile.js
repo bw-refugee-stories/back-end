643165027c10364dc23785ef3e2a5f3c93fd1768
Ian Bryant <48700036+IXLives@users.noreply.github.com>
2019-09-17 21:03:44 -0400

    Scaffolding

---
+++ knexfile.js
@@ -0,0 +1,27 @@
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    connection: { filename: './database/refugees.db3' },
+    useNullAsDefault: true,
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
+    migrations: {
+      directory: './database/migrations',
+      tableName: 'dbmigrations',
+    },
+    seeds: { directory: './database/seeds' },
+  },
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './database/test.db3',
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './database/migrations',
+    },
+  },
+};
