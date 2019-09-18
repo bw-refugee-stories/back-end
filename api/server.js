ecb07f144a386ce821f2da411c8df1a7cee89105
Ian Bryant <48700036+IXLives@users.noreply.github.com>
2019-09-17 21:06:17 -0400

    Basic server get created

--- api/server.js
+++ api/server.js
@@ -7,4 +7,8 @@ const server = express()
 server.use(express.json())
 server.use(cors())
 
+server.get('/', (req, res) => {
+    res.status(200).send('<h2>Refugee Stories</h2>')
+})
+
 module.exports = server
