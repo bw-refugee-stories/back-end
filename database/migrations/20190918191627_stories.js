66ec2374dc8de69b47f284ab660acaa9de6c6290
Ian Bryant <48700036+IXLives@users.noreply.github.com>
2019-09-18 21:01:49 -0400

    User-router created

--- database/migrations/20190918191627_stories.js
+++ database/migrations/20190918191627_stories.js
@@ -1,6 +1,15 @@
 
 exports.up = function(knex) {
   return knex.schema
+  .createTable('users', tbl => {
+    tbl.increments()
+    tbl.string('username')
+        .notNullable()
+        .unique()
+    tbl.string('password')
+        .notNullable()
+    tbl.string('role')
+})
     .createTable('stories' , tbl => {
       tbl.increments()
         .unique()
@@ -11,32 +20,33 @@ exports.up = function(knex) {
       tbl.string('name')
       tbl.string('email')
       tbl.boolean('pending')
-        .notNullable()  
+        .notNullable()
+      tbl.integer('user_id')
+        .unsigned()
+        .references('id')
+        .inTable('users')
   })
     .createTable('comments', tbl => {
         tbl.increments()
             .unique()
         tbl.string('contents')
+            .notNullable()
         tbl.integer('story_id')
             .unsigned()
             .notNullable()
             .references('id')
             .inTable('stories')
+        tbl.integer('user_id')
+            .unsigned()
+            .references('id')
+            .inTable('users')
     })
-    .createTable('users', tbl => {
-        tbl.increments()
-        tbl.string('username')
-            .notNullable()
-            .unique()
-        tbl.string('password')
-            .notNullable()
-        tbl.string('role')
-    })
+  
 };
 
 exports.down = function(knex) {
   return knex.schema
-    .dropTableIfExists('users')
     .dropTableIfExists('comments')
     .dropTableIfExists('stories')
+    .dropTableIfExists('users')  
 };
