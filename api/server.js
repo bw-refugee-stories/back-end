226fbbfbbfdeede00845ff64fefbd0435913338e
Ian Bryant <48700036+IXLives@users.noreply.github.com>
2019-09-18 20:21:53 -0400

    Server created and tested

--- api/server.js
+++ api/server.js
@@ -6,6 +6,7 @@ const server = express()
 
 server.use(express.json())
 server.use(cors())
+server.use(helmet())
 
 server.get('/', (req, res) => {
     res.status(200).send('<h2>Refugee Stories</h2>')
