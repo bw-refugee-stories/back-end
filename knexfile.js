ebba57495e9d2899c1aa68f79b8c7ef7874bcf0b
Ian Bryant <48700036+IXLives@users.noreply.github.com>
2019-09-28 11:00:29 -0400

    Seed data

--- knexfile.js
+++ knexfile.js
@@ -14,6 +14,21 @@ module.exports = {
     },
     seeds: { directory: './database/seeds' },
   },
+  production: {
+    client: 'sqlite3',
+    connection: { filename: './database/refugees.db3' },
+    useNullAsDefault: true,
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
+    migrations: {
+      directory: './database/migrations',
+      tableName: 'dbmigrations',
+    },
+    seeds: { directory: './database/seeds' },
+  },
   testing: {
     client: 'sqlite3',
     connection: {
